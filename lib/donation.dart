d44bf87dd00ae2113bfd0c4db3724f1368bb51c5
shahad <shahad2bh@outlook.com>
2025-11-12 12:42:13 +0300

    Create DonationPage

--- lib/donation.dart
+++ lib/donation.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DonationPage extends StatefulWidget {
+  const DonationPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(),
-    );
-  }
+  State<DonationPage> createState() => _DonationPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class _DonationPageState extends State<DonationPage> {
+  final _formKey = GlobalKey<FormState>();
+  final ImagePicker _imagePicker = ImagePicker();
+
+  // Form fields
+  String? selectedEquipmentType;
+  String? selectedCondition;
+  List<File> selectedImages = [];
+
+  final TextEditingController _itemNameController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  final List<String> equipmentTypes = [
+    'Wheelchair', 'Walker', 'Crutches', 'Hospital Bed',
+    'Oxygen Machine', 'Cane', 'Mobility Scooter', 'Other',
+  ];
+
+  final List<String> conditionStatus = [
+    'Excellent', 'Good', 'Fair', 'Needs Repair',
+  ];
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Scaffold()
-    );
+  void dispose() {
+    _itemNameController.dispose();
+    _descriptionController.dispose();
+    _locationController.dispose();
+    _quantityController.dispose();
+    super.dispose();
   }
 }
